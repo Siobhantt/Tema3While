25d0327ba3c7f1578b8be657b9cb6ab52c686517
Luisa <Luisa@192.168.0.153>
2022-10-25 11:07:49 +0200

    Ej1

---
+++ Tema3EjerciciosWhile/src/tema3ejercicioswhile/Ejercicio1.java
@@ -0,0 +1,35 @@
+package tema3ejercicioswhile;
+
+import java.util.Scanner;
+
+public class Ejercicio1 {
+
+	public static void main(String[] args) {
+		/*Declaramos las variables donde suma será donde guardaremos la suma final y 
+		 * num sera donde guardaremos el numero introducido por el usuario*/ 
+		int suma =0;
+		int num =0;
+		
+		//declaramos el scanner
+		Scanner lee = new Scanner(System.in);
+		
+		//Leemos el numero del usuario y lo almacenamos en num
+		System.out.println("Por favor introduzca los numeros a sumar (si quiere dejar de sumar introduzca un numero negativo): ");
+		num=lee.nextInt();
+		
+		//iniciamos un while donde la condicion sera que num sea mayor o igual a 0
+		
+		while(num>=0) {
+			//si se cumple a condicion en suma se guardará lo que introduzca el usuario
+			suma +=num;
+			//volveremos a leer lo que introduzca el usuario
+			num = lee.nextInt();	
+		}
+			//se imprime la suma total de todo lo que ha introducido el usuario
+		System.out.println("La suma de todos los numeros introducidos es: " + suma);
+		
+		//cerramos el scanner
+		lee.close();
+	}
+
+}
